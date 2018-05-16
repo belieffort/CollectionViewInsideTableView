c3d7859b4a3f9f89b25b1b48b714504f644dadf8
한병두 <ByungdooHan@hanbyeongduui-MacBook-Pro.local>
2018-05-16 23:35:48 +0900

    first commit

---
+++ collectionViewInsideTableView5/InsideCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  InsideCollectionViewCell.swift
+//  collectionViewInsideTableView5
+//
+//  Created by 한병두 on 2018. 5. 16..
+//  Copyright © 2018년 Byungdoo Han. All rights reserved.
+//
+
+import UIKit
+
+class InsideCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ collectionViewInsideTableView5/MainTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MainTableViewCell.swift
+//  collectionViewInsideTableView5
+//
+//  Created by 한병두 on 2018. 5. 16..
+//  Copyright © 2018년 Byungdoo Han. All rights reserved.
+//
+
+import UIKit
+
+class MainTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
